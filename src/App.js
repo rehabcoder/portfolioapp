7d8736a58d5753e585849bc98a7fbb8a281b53a8
unknown <akshay.saini@eaglecrk.com>
2018-04-22 22:37:30 -0400

    projects page added react section

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React, { Component } from 'react';
 import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
 import './App.css';
 import Main from './components/main';
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
-      <div style={{height: '300px', position: 'relative'}}>
+      <div>
     <Layout fixedHeader>
-        <Header title={<span><span style={{ color: '#ddd' }}>Area / </span><strong>The Title</strong></span>}>
+        <Header className="header-color" title={<span><span style={{ color: '#ddd' }}>Area / </span><strong>The Title</strong></span>}>
             <Navigation>
                 <Link to="/resume">Resume</Link>
                 <Link to="/aboutme">About Me</Link>
